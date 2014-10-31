451b948e536576b42d63c13714366e639f5648d8
zhengrunqiang <653260669@qq.com>
2014-10-31 17:23:24 +0800

    设计师源文件

---
+++ upload/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8080/GogoDesginer/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8080/GogoDesginer/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\Apache24\htdocs\GogoDesginer\upload/catalog/');
+define('DIR_SYSTEM', 'D:\Apache24\htdocs\GogoDesginer\upload/system/');
+define('DIR_LANGUAGE', 'D:\Apache24\htdocs\GogoDesginer\upload/catalog/language/');
+define('DIR_TEMPLATE', 'D:\Apache24\htdocs\GogoDesginer\upload/catalog/view/theme/');
+define('DIR_CONFIG', 'D:\Apache24\htdocs\GogoDesginer\upload/system/config/');
+define('DIR_IMAGE', 'D:\Apache24\htdocs\GogoDesginer\upload/image/');
+define('DIR_CACHE', 'D:\Apache24\htdocs\GogoDesginer\upload/system/cache/');
+define('DIR_DOWNLOAD', 'D:\Apache24\htdocs\GogoDesginer\upload/system/download/');
+define('DIR_UPLOAD', 'D:\Apache24\htdocs\GogoDesginer\upload/system/upload/');
+define('DIR_MODIFICATION', 'D:\Apache24\htdocs\GogoDesginer\upload/system/modification/');
+define('DIR_LOGS', 'D:\Apache24\htdocs\GogoDesginer\upload/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '111111');
+define('DB_DATABASE', 'gogodesigner');
+define('DB_PREFIX', '');
